fbb4a3ecbf8912eeb4a0874c17d99ffc110ddc40
Laurin Quast <laurinquast@googlemail.com>
2022-06-05 18:33:21 -0700

    fix: prettier config

--- packages/prettier-config/index.cjs
+++ packages/prettier-config/index.cjs
@@ -1,17 +1,4 @@
 module.exports = {
-  trailingComma: 'es5',
-  printWidth: 120,
-  singleQuote: true,
-  arrowParens: 'avoid',
-  overrides: [
-    {
-      files: '*.md{,x}',
-      options: {
-        semi: false,
-        trailingComma: 'none',
-      },
-    },
-  ],
   plugins: [
     // for prettifying shellscript, Dockerfile, properties, gitignore, dotenv
     'prettier-plugin-sh',
