ddcd4c32b5521e7496e56d55422fa6b79aa3dbe2
Laurin Quast <laurinquast@googlemail.com>
2022-07-04 13:37:53 +0200

    Revert "fix: prettier config (#7)"
    This reverts commit 36a2d58e49e26b5f6b5d95113d1024724eb344cd.

--- packages/prettier-config/index.cjs
+++ packages/prettier-config/index.cjs
@@ -1,4 +1,17 @@
 module.exports = {
+  trailingComma: 'es5',
+  printWidth: 120,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: '*.md{,x}',
+      options: {
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
   plugins: [
     // for prettifying shellscript, Dockerfile, properties, gitignore, dotenv
     require('prettier-plugin-sh'),
