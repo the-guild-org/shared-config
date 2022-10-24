bc56ef49b541fc0bcaecf9cc943cddafb05571fb
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-10-24 14:07:54 +0200

    remove unneeded tailwind `content` field patterns

--- packages/tailwind-config/index.cjs
+++ packages/tailwind-config/index.cjs
@@ -2,14 +2,6 @@ module.exports = {
   content: [
     './src/**/*.{tsx,mdx}',
     './theme.config.tsx',
-    '../node_modules/nextra-theme-docs/dist/**/*.js',
-    './node_modules/nextra-theme-docs/dist/**/*.js',
-    // components v3 (remove when full migration is done)
-    '../node_modules/@theguild/components/dist/index.mjs',
-    './node_modules/@theguild/components/dist/index.mjs',
-    // components v4 (remove when full migration is done)
-    '../node_modules/@theguild/components/esm/**/*.js',
-    './node_modules/@theguild/components/esm/**/*.js',
     // components v4 + tsup
     '../node_modules/@theguild/components/dist/**/*.{js,mjs}',
     './node_modules/@theguild/components/dist/**/*.{js,mjs}',
